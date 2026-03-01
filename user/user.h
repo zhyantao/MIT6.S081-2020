520960e4d5cb731a7cd62e9fa31cacb1d5a7a65b
zhyantao <yantao.z@outlook.com>
2026-03-01 21:28:25 +0000

    submit: Lab: mmap (hard)

--- user/user.h
+++ user/user.h
@@ -40,3 +40,5 @@ void free(void*);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
+void *mmap(void *, uint64, int, int, int, uint64);
+int munmap(void *, uint64);
